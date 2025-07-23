be9052b3febeb19ba93a9ba960fc1de92c3df8ac
ethery <loic.meunier74@gmail.com>
2025-07-23 18:39:03 +0200

    path find base

---
+++ Unity/Assets/Scripts/2DPathfinding/AStarCollector.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class AStarCollector : MonoBehaviour
+{
+	private char[][] matrix;
+	private Tilemap m_tilemap => GetComponent<Tilemap>();
+
+	public void Collect()
+	{
+		BoundsInt boundaries = m_tilemap.cellBounds;
+		Vector3Int testedPos = new Vector3Int();
+		for (int x = boundaries.xMin;x<boundaries.xMax;x++)
+		{
+			testedPos.x = x;
+			for(int y = boundaries.yMin; y<boundaries.yMax;y++)
+			{
+				testedPos.y = y;
+				if(Physics2D.Raycast(m_tilemap.CellToWorld(testedPos), Vector2.down))
+				{
+					jijio
+				}
+			}
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		
+	}
+}
